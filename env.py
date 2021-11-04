9b1cdcca191a0c5fcf21a7defbda864ea7d8959c
ellis882 <ellisrachel1304@me.com>
2021-11-04 15:25:34 +0000

    created heroku app, attached to database, prepared environment for project

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://nagjmwegmprdwk:0949f2d6f783a2222fcc01a259a0296c505f9d72d1967ece0ac9f8e2ca7dea8b@ec2-54-78-211-131.eu-west-1.compute.amazonaws.com:5432/d1et0gk6b2cc15"
+os.environ["SECRET_KEY"] = "randomSecretKey0411!!"
