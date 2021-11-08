c6d6146cb6ec9b9644613075d2251f8031c8a5ae
ellis882 <ellisrachel1304@me.com>
2021-11-08 11:58:42 +0000

    add authorisation, commenting

--- codestar/settings.py
+++ codestar/settings.py
@@ -43,11 +43,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'cloudinary_storage',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK= 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
